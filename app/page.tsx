ee9193add91ca3ad749a7589ee4917989a18ed0b
drdevlin <drdevlin@fastmail.com>
2023-05-29 20:43:50 -0400

    feat: add main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,91 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
 
-export default function Home() {
+import { useState } from 'react';
+import { getAuth, signOut } from 'firebase/auth';
+
+import { Nav, Page } from '@/components/Nav';
+import { RemarkList } from '@/components/RemarkList';
+import { Auth } from '@/components/Auth';
+import { CreateRemark } from '@/components/CreateRemark';
+
+import styles from './page.module.css';
+
+// Helpers
+const filterDateForPage = new Map<Page, Date>([
+  [Page.Recent, new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)], // 1 week ago
+  [Page.Hot, new Date(Date.now() - 1 * 24 * 60 * 60 * 1000)], // 1 day ago
+  // all other pages return undefined, which means no date filter
+]);
+
+// Component
+export default function Main() {
+  // State
+  const [selectedPage, setSelectedPage] = useState<Page>(Page.Home);
+
+  // Handlers
+  const handleHomeClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault();
+    setSelectedPage(Page.Home);
+  };
+
+  const handleRecentClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault();
+    setSelectedPage(Page.Recent);
+  };
+
+  const handleHotClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault();
+    setSelectedPage(Page.Hot);
+  };
+
+  const handleCreateClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault();
+
+    const auth = getAuth();
+    if (!auth.currentUser) return;
+
+    setSelectedPage(Page.Create);
+  };
+
+  const handleAuthClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault();
+
+    const auth = getAuth();
+    if (auth.currentUser) {
+      signOut(auth);
+      setSelectedPage(Page.Home);
+      return;
+    }
+
+    setSelectedPage(Page.Auth);
+  };
+
+  const handleSuccess = () => {
+    setSelectedPage(Page.Home);
+  }
+
+  // Calculated Props
+  const inRemarksMode = selectedPage === Page.Home
+                     || selectedPage === Page.Recent
+                     || selectedPage === Page.Hot;
+  const inAuthMode    = selectedPage === Page.Auth;
+  const inCreateMode  = selectedPage === Page.Create;
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <>
+      <Nav
+        selectedPage={selectedPage}
+        onHomeClick={handleHomeClick}
+        onRecentClick={handleRecentClick}
+        onHotClick={handleHotClick}
+        onCreateClick={handleCreateClick}
+        onAuthClick={handleAuthClick}
+      />
+      <main className={styles.main}>
+        {inRemarksMode && <RemarkList startDate={filterDateForPage.get(selectedPage)} />}
+        {inAuthMode    && <Auth onSuccess={handleSuccess}/>}
+        {inCreateMode  && <CreateRemark onSuccess={handleSuccess}/>}
+      </main>
+    </>
+  );
 }
